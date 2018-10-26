73ddd027eaf57814f2bbd5beca5cd4460c765d12
Briggan25 <44508930+Briggan25@users.noreply.github.com>
2018-10-26 15:21:35 -0400

    Added main file to Succ
    added main file to succ

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String args[])
+        throws InterruptedException {
+        String importantInfo[] = {
+            "W","a","k","e ","u","p, ", "N","e","o",".",".","."
+        };
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(150);
+            //Print a message
+            System.out.print(importantInfo[i]);
+        }
+
+
+        String importantInfo2[] = {
+            " ",
+            " "
+        };
+        for (int i = 0;
+             i < importantInfo2.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(1000);
+            //Print a message
+            System.out.println(importantInfo2[i]);
+        }
+        
+        
+        
+        
+        String importantInfo3[] = {
+            "h","i"
+        };
+        for (int i = 0;
+             i < importantInfo3.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(150
+            
+            
+            
+            );
+            //Print a message
+            System.out.print(importantInfo3[i]);
+        }
+    }
+}
