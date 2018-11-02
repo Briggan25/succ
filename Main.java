2f15bbb10c0a1d6e8929fc70bfffd2c71892b127
Briggan25 <44508930+Briggan25@users.noreply.github.com>
2018-11-02 15:08:23 -0400

    gh
    gf

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;  
 public class Main {
   public static void main(String args[]){                       
-          String s = "Hello, This is, Data, I am a poorly made chat bot.";  
+          throws InterruptedException {String importantInfo[] = {} "H","e","l","l","o","."," ","T","h","i","s"," ","i","s"," ","D","a","t","a","."," ","I"," ","a","m"," ","a"," ","p","o","o","r","l","y"," ","m","a","d","e"," ","c","h","a","t"," ","b","o","t",".";  
           //Create scanner Object and pass string in it  
           Scanner scan = new Scanner(s);  
           //Check if the scanner has a token  
@@ -10,10 +10,14 @@ public class Main {
           System.out.println("String: " +scan.nextLine());  
           scan.close();          
           Scanner in = new Scanner(System.in);  
-          System.out.print("What is your Name?");    
+          System.out.println("What is your Name?");    
           String name = in.next();              
-          System.out.print("How old are you, "+name+"?");  
-          int age = in.nextInt();            
+          System.out.println("How old are you, "+name+"?");  
+          int age = in.nextInt(); 
+          System.out.println("Are you a boy or a girl?");
+          String gender = in.next();     
+          System.out.println("");
+          String = in.next();         
           in.close();  
   }
 }
