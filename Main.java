011e8ba9a8b3908b499ada86b3115f3f3dd3d05a
hankg10 <44508992+hankg10@users.noreply.github.com>
2018-11-16 14:31:52 -0500

    Sexy man
    sos

--- Main.java
+++ Main.java
@@ -12,14 +12,11 @@ public class Main {
           Scanner in = new Scanner(System.in);  
           System.out.print("What is your Name?");    
           String name = in.next();
-          System.out.print("Hello"+" "+ name + " ");              
-          System.out.print("How old are you?");  
-=======
-          String name = in.next();              
+          System.out.print("Hello"+" "+ name + " ");                          
           System.out.print("How old are you, "+name+"?");  
           int age = in.nextInt();
           if (nextin.equals("Who are you?") || nextin.equals("Who are you") || nextin.equals("who are you") || nextin.equals("who are you?")){
-            String answer1 = "A poorly made chat bot, I said that didn't I?"
+            String answer1 = "A poorly made chat bot, I said that didn't I?";
           }
         in.close();  
   }
